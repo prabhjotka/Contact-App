5f1e33f34ee15c74e84f7d75bb5b84e328bddd82
Prabh <sonukaur68@gmail.com>
2023-12-14 01:57:22 -0700

    add search functionality

--- src/componenets/App.js
+++ src/componenets/App.js
@@ -12,7 +12,8 @@ import EditContact from "./EditContact";
 function App() {
   const LOCAL_STROAGE_KEY = "Contacts"
   const [contacts, setContacts] = useState([]);
-
+  const [searchTerm,setSearchTerm]=useState("");
+  const [searchResult,setSearchResult]=useState([]);
   const reteriveContacts=async ()=>
   {
     const response=await api.get("/contacts");
@@ -51,6 +52,22 @@ function App() {
     });
     setContacts(newContactList);
   }
+  const searchHandler=function(searchTerm)
+  {
+     setSearchTerm(searchTerm);
+    if(searchTerm!==""){
+      const newContactList=contacts.filter((contact)=>{
+        return Object.values(contact)
+        .join(" ").toLowerCase()
+        .includes(searchTerm.toLowerCase());
+      });
+      setSearchResult(newContactList);
+    }
+    else{
+      setSearchResult(contacts);
+    }
+
+  }
 
   useEffect(() => {
     // const getContacts = JSON.parse(localStorage.getItem(LOCAL_STROAGE_KEY));
@@ -73,8 +90,10 @@ function App() {
         <Route path="/add" element={<AddContact contactHandler={contactHandler} />} />
         <Route path="/"
           element={<ContactList
-            contacts={contacts}
+            contacts={searchTerm.length <1 ? contacts : searchResult}
             getContactid={removeContact}
+            searchTerm={searchTerm}
+            searchKeyword={searchHandler}
           />} />
            <Route path="/edit" element={<EditContact updateContactHandler={updateContactHandler} />} />
           <Route path="/Contact/:id" element={<ContactDetails/>}/>
--- src/componenets/ContactList.js
+++ src/componenets/ContactList.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ContactCard from "./ContactCard";
 import { Link } from "react-router-dom";
+import { useRef } from "react";
 const ContactList = function(props) {
-
+    const inputEl = useRef();
     const deleteContact = function(id) {
         props.getContactid(id);
     }
-
-    const renderList = props.contacts.map((contact,index) => {
+    const getSearchTerm = function() {
+        props.searchKeyword(inputEl.current.value);
+    }
+    const renderList = props.contacts.map((contact, index) => {
         return (
             <ContactCard
                 key={index}
@@ -19,12 +22,25 @@ const ContactList = function(props) {
     return (
         <div className="main">
             <h2>Contact List</h2>
+            <br />
+            <div className="ui search">
+                <div className="ui icon input">
+                    <input
+                        ref={inputEl}
+                        type="text"
+                        value={props.searchTerm}
+                        onChange={getSearchTerm}
+                        placeholder="search contact" className="prompt" />
+                    <i className="search icon"></i>
+                </div>
+            </div>
+            <br />
             <Link to="/add">
                 <button className="ui button blue right">Add Contact</button>
             </Link>
 
-            <div className="ui celled list">
-                {renderList}
+            <div className="ui celled list" >
+                {renderList.length > 0 ? renderList : " No Contact Available"}
             </div>
         </div>
 
